37c09946cbdb3cc7a58edcd0eecf89ac12dc34b4
Bernhard Fisseni <bernhard.fisseni@mail.de>
2020-03-21 23:27:36 +0100

    cleaner update

--- crawler/crawl_infektionsschutz.pl
+++ crawler/crawl_infektionsschutz.pl
@@ -16,7 +16,7 @@
 #      VERSION: 1.0
 #      CREATED: 2020-03-21, 17:03:33 (CET)
 #     REVISION: ---
-#  Last Change: 2020-03-21, 23:24:45 (+01:00)
+#  Last Change: 2020-03-21, 23:27:31 (+01:00)
 #===============================================================================
 
 use strict;
@@ -118,8 +118,8 @@ sub finish($params) {
         }
     );
     $params->{html}->find("script,stylesheet")->each(sub { $_->remove()});
-    $params->{html}->find("i")->each(sub{ $_->tag("em")});
-    $params->{html}->find("b")->each(sub{ $_->tag("strongs")});
+    $params->{html}->find("i")->each(sub {$_->tag("em")});
+    $params->{html}->find("b")->each(sub {$_->tag("strongs")});
     $params->{html}->find("*")->each(sub {if ($_->all_text() =~ m/\A\s*\Z/m){ $_->remove()}});
     $params->{text} = normalize_space(
         $plain->process($params->{html}->to_string()));
