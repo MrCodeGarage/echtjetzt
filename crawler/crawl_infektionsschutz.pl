3bf0233be6a330e4e399381ce9fbaf71ca376901
Bernhard Fisseni <bernhard.fisseni@mail.de>
2020-03-21 20:21:30 +0100

    cleaner update

--- crawler/crawl_infektionsschutz.pl
+++ crawler/crawl_infektionsschutz.pl
@@ -16,7 +16,7 @@
 #      VERSION: 1.0
 #      CREATED: 2020-03-21, 17:03:33 (CET)
 #     REVISION: ---
-#  Last Change: 2020-03-21, 18:18:43 (CET)
+#  Last Change: 2020-03-21, 20:20:50 (CET)
 #===============================================================================
 
 use strict;
@@ -37,10 +37,13 @@ use feature qw(say state switch unicode_strings signatures);
 no warnings qw(experimental::signatures);
 use re "/u";
 use autodie;
+use URI;
 use HTTP::Tiny;
 use HTML::Restrict;
+use Mojo::DOM;
 my $ua = HTTP::Tiny->new();
 
+use Data::Dump qw/dump/;
 
 my %DISPATCH = (
     "www.infektionsschutz.de" => [
@@ -50,26 +53,37 @@ my %DISPATCH = (
 );
 
 
-sub get_text($url) {
+sub get_document($url_string, $recursive) {
+  my $url = URI->new($url_string);
   my $response = $ua->get($url);
   if ($response->{success}) {
       my $tree = Mojo::DOM -> new($response->{content});
-      clean_infektionsschutz({
+      my $doc = get_main({
+              url => $url,
               html => $tree,
-              recursive => 1,
+              recursive => $recursive,
               links => [],
               metadata => [],
               header => $response->{headers},
           });
+      $doc = get_links($doc);
+      dump($doc->{links});
   }
+}
+sub is_local_link($url_string, $domain) {
+    my $base = $domain->scheme() . "://" . $domain->host();
+    my $rel_url = URI->new($url_string)->rel($base);
+    my $url = URI->new($url_string)->abs($base);
+    return ($url =~ m/$base/) && ($rel_url !~ m{^/+$});
 }
 
 sub get_links($params) {
-    $params->{links} = $params->{html}->find("a")->map(attr => "src")->compact()->to_array();
+    $params->{links} = $params->{html}
+        ->find("a")->map(attr => "href")->grep(sub{is_local_link($_, $params->{url})})->compact()->to_array();
     return $params;
 }
 
-sub strip($params) {
+sub strip($params) {
     my $plain = HTML::Restrict->new();
     my $moderate = HTML::Restrict-> new([
             rules => [
@@ -78,8 +92,8 @@ sub strip($params) {
             ]
         ]);
     $params->html->find("i")->each(sub{ $_->tag("em")});
-    $params->{plain} = $plain->process($params->{html});
-    $params->{html} = $moderate->process($params->{html});
+    $params->{html} = $plain->process($params->{html}->to_string());
+    $params->{html} = $moderate->process($params->{html}->to_string());
     return $params;
 }
 
@@ -87,9 +101,10 @@ sub strip($params) {
 # heuristically get a main part
 sub get_main($params) {
     my $main = $params->{html}->at('main') //
-        $params->{html}->at('div[class=main]') //
-        $params->{html}->at('div[class=content]');
+    $params->{html}->at('div[class=main]') //
+    $params->{html}->at('div[class=content]');
     $params->{html} = $main;
     return $params;
 }
 
+get_document("https://www.infektionsschutz.de/coronavirus/", 0);
