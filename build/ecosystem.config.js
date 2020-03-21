1efabffc4636d1fe4063f70c4b513eb87ca5c03a
Asan Stefanski <asan.stefanski@advisori.de>
2020-03-21 14:33:59 +0100

    update

--- build/ecosystem.config.js
+++ build/ecosystem.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-    apps : [{
-      name: 'k1',
-      script: '/usr/src/app/bundle/main.js',
-      args: '',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      output: './out.log',
+    apps: [{
+        name: 'k1',
+        script: '/usr/src/app/bundle/main.js',
+        args: '',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        output: './out.log',
         error: './error.log',
-          log: './combined.outerr.log',
-      env: {
-        NODE_ENV:"production",
-        PORT:"3400"
-      },
-      env_production: {
-        NODE_ENV:"production",
-        PORT:"3400"
-      }
+        log: './combined.outerr.log',
+        env: {
+            MONGO_URL: "mongodb+srv://hackaton:R8kiR0e3XhIqaVqp@cluster0.yiba8.mongodb.net/ki?retryWrites=true&w=majority",
+            ROOT_URL: "http://echtjetzt.code-garage-solutions.de",
+            NODE_ENV: "production",
+            PORT: "3400"
+        },
+        env_production: {
+            NODE_ENV: "production",
+            PORT: "3400"
+        }
     }],
-  
-    deploy : {
-      
+
+    deploy: {
+
     }
-  }
-  
+}
