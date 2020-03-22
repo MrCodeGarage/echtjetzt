90d39515a53e827d0140e00d7f3b6d9cd3378282
pfafft <tanja.pfaffel@msg.group>
2020-03-22 17:15:52 +0100

    find similar docs 2

--- python/nlp/find_similar_doc.py
+++ python/nlp/find_similar_doc.py
@@ -6,30 +6,27 @@ Created on Sat Mar 21 21:43:29 2020
 
 #!pip install sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import metrics
+from sklearn.metrics.pairwise import linear_kernel
 
 #input: inputtext
 #validated_data: texte aus validierten Quellen in numpy liste
-
 def find_similar_doc(input_data,validated_data):
     #Definiere "unwichtige" Wörter, die nicht gezählt werden
     my_stop_words = ['und', 'auf', 'zu', 'in', 'ist', 'der', 'die', 'das', 'wird','werden', 'hat', 'haben', 'dass', 'wie', 'von', 'oder', 'eine', 'ein', 'einer']
     #Definiere TfidfVectorizer
     tfidf_vectorizer = TfidfVectorizer(stop_words = my_stop_words)
-    # Vektorisiere Inputtext
-    tfidf_input = tfidf_vectorizer.fit_transform(input_data)
     # Vektorisiere validated Text
-    tfidf_validated = tfidf_vectorizer.transform(list(validated_data.keys()))
+    tfidf_validated = tfidf_vectorizer.fit_transform([i['text'] for i in validated_data])
+    # Vektorisiere Inputtext
+    tfidf_input = tfidf_vectorizer.transform(input_data)
+    
     
     #Berechne Ähnlichkeiten zwischen Inputtext und den vertrauenswürdigen Texten über Cosinus
-    cosine_similarities = metrics.pairwise.linear_kernel(tfidf_input, tfidf_validated).flatten()
+    cosine_similarities = linear_kernel(tfidf_input, tfidf_validated).flatten()
     # Finde Ähnlichkeitswert von besten 2 Dokumenten
     similarity_values = [sorted(cosine_similarities)[-1],sorted(cosine_similarities)[-2]]
     #Finde webstie von am besten passenden Dokument
-    dummy_dict = dict(zip(validated_data.keys(),cosine_similarities.argsort()))
-    similar_website = [validated_data[max(dummy_dict, key=dummy_dict.get)]]
-    del dummy_dict[max(dummy_dict, key=dummy_dict.get)]
-    similar_website.append(validated_data[max(dummy_dict, key=dummy_dict.get)])    
+    similar_website = [validated_data[cosine_similarities.argsort()[-1]]['link'],validated_data[cosine_similarities.argsort()[-2]]['link']]  
     
     #Gibt den text des ähnlichsten Dokuments zurück
     return (similar_website,similarity_values)
--- python/nlp/nlp_api.py
+++ python/nlp/nlp_api.py
@@ -1,21 +1,19 @@
 from datetime import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import metrics
+from sklearn.metrics.pairwise import linear_kernel
 
 from db_api import get_articles
-
 from nlp.find_similar_doc import find_similar_doc
-
 from nlp.linear_regression_predict import predict
 
 def classify_text(text: str):
     percent = predict(text)
 
     articles = get_articles()
-    nurText = dict(map(lambda el: el["text"], articles))
-    print(nurText)
-    similar_website, similarity_score = find_similar_doc([text], nurText)
-    #print(similar_website, similarity_score)
+    #nurText = list(map(lambda el: el["text"], articles))
+
+    similar_website, similarity_score = find_similar_doc([text], articles)
+    
         
     return {
         "percent": percent,
