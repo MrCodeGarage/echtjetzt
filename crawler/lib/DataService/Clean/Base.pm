15a6c3e16eec3be90f9eae822101d2877061c004
Bernhard Fisseni <bernhard.fisseni@mail.de>
2020-03-22 18:06:03 +0100

    fixed magic

--- crawler/lib/DataService/Clean/Base.pm
+++ crawler/lib/DataService/Clean/Base.pm
@@ -139,7 +139,7 @@ sub get_links($params) {
   my $link_candidates = $params->{html}
   ->find("a")->map(attr => "href")->compact()
   ->grep(\&is_www_link)
-  ->map(sub{s/\#.*$//g})
+  ->map(sub{s/\#.*$//g; return $_})
   ->grep(\&is_licit_link)
   ->map(sub {
     return URI->new($_)->abs($params->{base_url})->as_string();
